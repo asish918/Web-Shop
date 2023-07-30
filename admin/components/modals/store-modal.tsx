97f736235ca1ed95cd37368bfa3f4b9a984c1be5
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-07-31 01:53:44 +0530

    PlanetScale MySQL setup done

--- admin/components/modals/store-modal.tsx
+++ admin/components/modals/store-modal.tsx
@@ -8,6 +8,8 @@ import { useForm } from "react-hook-form";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components//ui/button";
+import { useState } from "react";
+import axios from "axios";
 
 const formSchema = z.object({
     name: z.string().min(1),
@@ -15,6 +17,7 @@ const formSchema = z.object({
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
+    const [loading, setLoading] = useState(false);
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -24,7 +27,17 @@ export const StoreModal = () => {
     });
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        //TODO: Create Store
+        try {
+            setLoading(true);
+
+            const response = await axios.post('/api/stores', values);
+
+            console.log(response.data)
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
@@ -41,15 +54,15 @@ export const StoreModal = () => {
                                 <FormItem>
                                     <FormLabel>Name: </FormLabel>
                                     <FormControl>
-                                        <Input placeholder="E-Commerce" {...field} />
+                                        <Input disabled={loading} placeholder="E-Commerce" {...field} />
                                     </FormControl>
                                     <FormMessage />
                                 </FormItem>
                             )} />
 
                             <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                                <Button variant={"outline"} onClick={storeModal.onClose}>Cancel</Button>
-                                <Button type="submit">Continue</Button>
+                                <Button disabled={loading} variant={"outline"} onClick={storeModal.onClose}>Cancel</Button>
+                                <Button disabled={loading} type="submit">Continue</Button>
                             </div>
                         </form>
                     </Form>
