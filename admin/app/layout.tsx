3ea37d83095ed602488ea7d3467e230b84919944
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-12-13 11:43:01 +0530

    Store Creation API and Modal done

--- admin/app/layout.tsx
+++ admin/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ModalProvider />
-            {children}
+          {children}
         </body>
       </html>
     </ClerkProvider>
