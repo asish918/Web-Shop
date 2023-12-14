a6af921edd6c4cfa6926922a45469578da1f3103
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-12-14 12:18:29 +0530

    Added Product Listing and Cart functionality

--- store-front/app/(routes)/page.tsx
+++ store-front/app/(routes)/page.tsx
@@ -2,10 +2,13 @@ import Image from 'next/image'
 import Billboard from "@/components/ui/billboard";
 import getBillboard from "@/actions/get-billboard";
 import Container from "@/components/ui/container";
+import ProductList from '@/components/product-list';
+import getProducts from '@/actions/get-products';
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("6b0edb2c-c3dd-493b-a4a0-87c30ab6a9e8");
 
   return (
@@ -14,6 +17,9 @@ const HomePage = async () => {
         <Billboard
           data={billboard}
         />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   )
--- store-front/next.config.js
+++ store-front/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "res.cloudinary.com",
+            "tailwindui.com"
+        ]
+    }
+}
 
 module.exports = nextConfig
